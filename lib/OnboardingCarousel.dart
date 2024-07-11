6adddb8da05f030829788108c5b1e3d3bad4f5ec
JohnGastone <johngastone11@gmail.com>
2024-07-11 17:10:41 +0300

    Onboarding carousel

--- lib/OnboardingCarousel.dart
+++ lib/OnboardingCarousel.dart
@@ -1,29 +1,186 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: file_names
 
-import "package:flutter/material.dart";
-import "package:google_fonts/google_fonts.dart";
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Onboardingcarousel extends StatefulWidget {
+class Onboardingcarousel extends StatelessWidget {
+  final Color kDarkBlueColor = const Color.fromARGB(255, 83, 45, 8);
+
   const Onboardingcarousel({super.key});
 
   @override
-  State<Onboardingcarousel> createState() => _OnboardingcarouselState();
-}
-
-class _OnboardingcarouselState extends State<Onboardingcarousel> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Welcome to Dokta Konzo App',
-                style: GoogleFonts.poppins(fontSize: 20)),
-          ],
+    return OnBoardingSlider(
+      finishButtonText: 'Register',
+      onFinish: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => const Onboardingcarousel(),
+          ),
+        );
+      },
+      finishButtonStyle: FinishButtonStyle(
+        backgroundColor: kDarkBlueColor,
+      ),
+      skipTextButton: Text(
+        'Skip',
+        style: GoogleFonts.poppins(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
         ),
       ),
+      trailing: Text(
+        'Login',
+        style: GoogleFonts.poppins(
+          fontSize: 16,
+          color: kDarkBlueColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailingFunction: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => const Onboardingcarousel(),
+          ),
+        );
+      },
+      controllerColor: kDarkBlueColor,
+      totalPage: 3,
+      headerBackgroundColor: Colors.white,
+      pageBackgroundColor: Colors.white,
+      background: [
+        Image.asset(
+          'assets/111.png',
+          height: 400,
+          width: 400,
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'assets/222.png',
+          height: 400,
+          width: 400,
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'assets/333.png',
+          height: 400,
+          width: 400,
+          fit: BoxFit.cover,
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 450,
+              ),
+              Text(
+                'Welcome to Alama',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: kDarkBlueColor,
+                  fontSize: 34.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Discover the finest Tanzanian cuisine and enjoy a seamless dining experience',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.blueGrey,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Easy Ordering.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: kDarkBlueColor,
+                  fontSize: 34.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Browse our extensive menu and place your order with just few taps',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.blueGrey,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 450,
+              ),
+              Text(
+                'Seamless Payments',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: kDarkBlueColor,
+                  fontSize: 33.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Choose from a variety of payment options, including Lipa no and cash.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.blueGrey,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
