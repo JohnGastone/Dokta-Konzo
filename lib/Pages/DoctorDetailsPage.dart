aa9f8a3b3a56aedaaf6a09fb3bcf6f5940767fe0
JohnGastone <johngastone11@gmail.com>
2024-07-12 13:50:21 +0300

    +++

--- lib/Pages/DoctorDetailsPage.dart
+++ lib/Pages/DoctorDetailsPage.dart
@@ -114,31 +114,79 @@ class _DoctorDetailsPageState extends State<DoctorDetailsPage> {
                   height: 20,
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  child: Row(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
                     children: [
-                      Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "About",
-                            style: GoogleFonts.poppins(
-                                fontSize: 22, color: Colors.grey),
+                          Column(
+                            children: [
+                              Text(
+                                "About",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 22, color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                height: 3,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10)),
+                              )
+                            ],
                           ),
-                          SizedBox(
-                            height: 10,
+                          Column(
+                            children: [
+                              Text(
+                                "Review",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 22, color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                height: 3,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10)),
+                              )
+                            ],
                           ),
-                          Container(
-                            height: 3,
-                            width: 50,
-                            decoration: BoxDecoration(
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.circular(10)),
+                          Column(
+                            children: [
+                              Text(
+                                "Rating",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 22, color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                height: 3,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10)),
+                              )
+                            ],
                           )
                         ],
+                      ),
+                      Text(
+                        "Dr Maryam Mahwaya is the greatest cardiology specialist in the country since 2010. She has achieved several successful awards both in Tanzania and at international level for her wonderful contribution in the field...",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15, color: Colors.grey),
                       )
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
