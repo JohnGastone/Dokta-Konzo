cc627948d8dd1f2fdaf78f9acbbc4cede8b21566
JohnGastone <johngastone11@gmail.com>
2024-07-12 13:15:51 +0300

    +++

--- lib/Pages/DoctorDetailsPage.dart
+++ lib/Pages/DoctorDetailsPage.dart
@@ -17,21 +17,109 @@ class _DoctorDetailsPageState extends State<DoctorDetailsPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.grey,
-              radius: 20,
-              child: Icon(Icons.arrow_back_ios),
-            )
-          ],
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 20,
+                child: Center(
+                    child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )),
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              Text(
+                "Doctor Details",
+                style: GoogleFonts.poppins(
+                    fontSize: 20, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
         ),
       ),
       body: Stack(
         children: [
           SingleChildScrollView(
             child: Column(
-              children: [],
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 10, right: 10),
+                  child: Container(
+                    height: 150,
+                    width: 340,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 110,
+                            width: 110,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage("./assets/d2.jpeg")),
+                                borderRadius: BorderRadius.circular(15)),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Dr. Maryam Mahwaya",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 19, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Wakombozi hospital",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16, fontWeight: FontWeight.w500),
+                              ),
+                              Text("Cardiology",
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 15, color: Colors.grey)),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.clock,
+                                    color:
+                                        const Color.fromARGB(255, 90, 9, 104),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "4:30 PM - 7:30 PM",
+                                    style: GoogleFonts.poppins(fontSize: 16),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Align(
