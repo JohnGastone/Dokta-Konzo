6c0ee6e8da645e0729e54e68513694bbf786577e
JohnGastone <johngastone11@gmail.com>
2024-07-14 18:04:42 +0300

    Appointments page

---
+++ lib/Pages/Appointments.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AppointmentsPage extends StatefulWidget {
+  const AppointmentsPage({super.key});
+
+  @override
+  State<AppointmentsPage> createState() => _AppointmentsPageState();
+}
+
+class _AppointmentsPageState extends State<AppointmentsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
