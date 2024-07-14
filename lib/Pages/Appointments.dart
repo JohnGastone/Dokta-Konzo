86ec64ba4d4920a835b73248a2f496721f686213
JohnGastone <johngastone11@gmail.com>
2024-07-14 18:39:22 +0300

    +++

--- lib/Pages/Appointments.dart
+++ lib/Pages/Appointments.dart
@@ -51,7 +51,89 @@ class _AppointmentsPageState extends State<AppointmentsPage> {
                 "My Appointments",
                 style: GoogleFonts.poppins(
                     fontSize: 23, fontWeight: FontWeight.w500),
-              )
+              ),
+              Container(
+                height: 130,
+                width: 330,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color.fromARGB(255, 239, 235, 235),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 110,
+                        width: 110,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage("./assets/d2.jpeg")),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.purpleAccent,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  CupertinoIcons.phone,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Voice Call - Accepted",
+                            style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "Dr. Maryam Mahwaya",
+                            style: GoogleFonts.poppins(
+                                fontSize: 19, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.clock,
+                                color: const Color.fromARGB(255, 90, 9, 104),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "4:30 PM - 7:30 PM",
+                                style: GoogleFonts.poppins(fontSize: 16),
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
