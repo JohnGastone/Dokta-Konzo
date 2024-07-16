ce8f1803e5d78d9c0ce00ef33aabdbda9eb87432
JohnGastone <johngastone11@gmail.com>
2024-07-16 22:29:17 +0300

    Appointment page++

---
+++ lib/models.dart
@@ -0,0 +1,28 @@
+class AppointmentTimesModel {
+  String? time;
+  AppointmentTimesModel(this.time);
+}
+
+class AppointmentMorningTimesList {
+  static List<AppointmentTimesModel> getAppointmentTimes = [
+    AppointmentTimesModel('09:00 AM'),
+    AppointmentTimesModel('09:30 AM'),
+    AppointmentTimesModel('10:00 AM'),
+    AppointmentTimesModel('10:30 AM'),
+    AppointmentTimesModel('11:00 AM'),
+    AppointmentTimesModel('11:30 AM'),
+  ];
+  static List<AppointmentTimesModel> displayList =
+      List.from(AppointmentMorningTimesList.getAppointmentTimes);
+}
+
+class AppointmentEveningTimesList {
+  static List<AppointmentTimesModel> getAppointmentTimes = [
+    AppointmentTimesModel('05:00 PM'),
+    AppointmentTimesModel('05:30 PM'),
+    AppointmentTimesModel('06:00 PM'),
+    AppointmentTimesModel('06:30 PM'),
+  ];
+  static List<AppointmentTimesModel> displayList =
+      List.from(AppointmentEveningTimesList.getAppointmentTimes);
+}
