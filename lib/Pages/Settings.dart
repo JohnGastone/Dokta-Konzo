9d3653b593ab39c9e4c1079db0c33af2fc8e0758
JohnGastone <johngastone11@gmail.com>
2024-07-20 13:22:28 +0300

    Settings page +++

--- lib/Pages/Settings.dart
+++ lib/Pages/Settings.dart
@@ -1,15 +1,47 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SettingsPages extends StatefulWidget {
+  const SettingsPages({super.key});
 
   @override
-  State<SettingsPage> createState() => _SettingsPageState();
+  State<SettingsPages> createState() => _SettingsPagesState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
+class _SettingsPagesState extends State<SettingsPages> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ClipOval(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("./assets/p2.png"),
+                  radius: 28,
+                ),
+              ),
+              Text(
+                "Settings",
+                style: GoogleFonts.poppins(
+                    fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
