819976e9e4ccbceb7aa397eb336cc176a999d585
JohnGastone <johngastone11@gmail.com>
2024-07-20 13:19:24 +0300

    Settings page +++

--- lib/Pages/Appointment/Appointments.dart
+++ lib/Pages/Appointment/Appointments.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:doktakonzo/Pages/Appointment/Appointment.dart';
+import 'package:doktakonzo/Pages/Appointment/NewAppointment.dart';
 import 'package:doktakonzo/Pages/components/DatePicker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
